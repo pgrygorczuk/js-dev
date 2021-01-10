a397cb325995794c10d818d018376760076c970e
pgrygorczuk <pawelgrygorczuk@poczta.onet.pl>
2021-01-10 18:10:28 +0100

    React: lab4 | zad3: zsynchronizowane inputy

--- react/lab4/my-app/src/LiftUpStateApp.js
+++ react/lab4/my-app/src/LiftUpStateApp.js
@@ -1,5 +1,10 @@
 import * as React from 'react';
 
+const scaleNames = {
+    c: "Celsjuszach",
+    f: "Fahrenheitach",
+};
+
 function BoilingVerdict(props)
 {
     if( props.celsius >= 100 ){
@@ -9,16 +14,38 @@ function BoilingVerdict(props)
     }
 }
 
+function TemperatureInput(props)
+{
+    const [celsius, setCelsius] = React.useState( '0' );
+    
+    return (
+        <React.Fragment>
+            <label>Podaj temperature {scaleNames[props.scale]}</label>
+            <input
+                value={props.value}
+                onChange={e => props.setTemperature(e.target.value)} />
+        </React.Fragment>
+    );
+}
+
 function App()
 {
-    const [temperature, setTemperature] = React.useState('');
+    const [celsius, setCelsius] = React.useState(0);
 
     return (
         <div>
-            <input
-                value={temperature}
-                onChange={e => setTemperature(e.target.value)} />
-            <BoilingVerdict celsius={parseFloat(temperature)} />
+            <TemperatureInput
+                scale="c"
+                value={celsius}
+                setTemperature = {setCelsius} />
+            <br />
+            <TemperatureInput
+                scale="f"
+                value={celsius}
+                setTemperature={setCelsius}
+            />
+            
+            <BoilingVerdict celsius={parseFloat(celsius)} />
         </div>
     );
 }
