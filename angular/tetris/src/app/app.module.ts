54c5cb2039b5ce22cd440b19cb441dc8fc6657d8
pgrygorczuk <pawelgrygorczuk@poczta.onet.pl>
2021-02-09 18:25:48 +0100

    Tetris | Game component added.

--- angular/tetris/src/app/app.module.ts
+++ angular/tetris/src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { GameComponent } from './game/game.component';
+import {TetrisCoreModule} from 'ngx-tetris';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GameComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    TetrisCoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
