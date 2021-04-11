2ecbb6eb7803faf586c9e9f0749d5fbdc0441891
pgrygorczuk <pawelgrygorczuk@poczta.onet.pl>
2021-04-11 17:39:10 +0200

    Node.js | lab8: zad4

---
+++ nodejs/lab8/zad4.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const fs = require('fs');
+const path = require('path');
+
+const file = (req, res, next) => {
+    const file = req.params.file;
+    if( fs.existsSync(file) ){
+        res.sendFile( path.resolve(file) );
+    }
+    else{
+        res.send(404, 'File does not exists');
+    }
+    next();
+};
+
+app.use('/:file', file);
+
+app.get('/:file', (req, res) => {
+    //res.send(req.params.file);
+});
+
+app.listen(4700, () => console.log('Server started.'));
