f2c4f3871f8db3fbf9d39e79c24cb6a7ed6bbe6b
pgrygorczuk <pawelgrygorczuk@poczta.onet.pl>
2021-05-23 21:24:12 +0200

    React | useReducer in TodoRow

--- react/todo-app/src/containers/TodoRow.js
+++ react/todo-app/src/containers/TodoRow.js
@@ -1,15 +1,40 @@
 import * as React from 'react';
 import { Button } from '../components/Button.js';
 
+
 export function TodoRow({task, index, onDelete, onEdit, onCheck})
 {
-    const [editMode, setEditMode] = React.useState(false);
+    const initialState = {
+        editMode: false
+    };
 
-    function handleKeyPress(e){
-        if( e.key === 'Enter' ){
-            setEditMode(false);
+    function reducer(state, action){
+        //console.log( state, action );
+        switch(action.type){
+            case 'EDIT':
+                return {
+                    ...state,
+                    editMode: action.payload,
+                };
+            case 'DELETE':
+                onDelete(action.payload);
+                return state;
+            default:
+                return state;
         }
     }
+
+    const [state, dispatch] = React.useReducer(reducer, initialState);
+
+    function edit(){
+        dispatch({ type: 'EDIT', payload: !state.editMode });
+    }
+
+    function del(index){
+        dispatch({ type: 'DELETE', payload: index });
+    }
+
+    //console.log(state);
 
     return (
         <tr key={index}>
@@ -21,17 +46,19 @@ export function TodoRow({task, index, onDelete, onEdit, onCheck})
                 {/* {task.index} */}
             </td>
             <td>
-                {editMode ?
+                {state.editMode ?
                     <input type="text" value={task.name}
                         onChange={e => onEdit(task.index, e.target.value)}
-                        onKeyPress={e => handleKeyPress(e)} />
+                        onKeyPress={e => e.key==='Enter'? dispatch({ type: 'EDIT' }) : undefined} />
                     :
                     task.name
                 }
             </td>
             <td>
-                <Button iconClass="bi bi-pencil" onClick={e => setEditMode(!editMode)} />
-                <Button iconClass="bi bi-trash" onClick={e => onDelete(task.index)} />
+                <Button iconClass="bi bi-pencil" onClick={ e => edit() } />
+                <Button iconClass="bi bi-trash" onClick={ e => del(task.index) } />
+                {/* <Button iconClass="bi bi-pencil" onClick={e => setEditMode(!editMode)} />
+                <Button iconClass="bi bi-trash" onClick={e => onDelete(task.index)} /> */}
             </td>
         </tr>
     );
